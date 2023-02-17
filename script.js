a559855eccaef08723ba092a385e2925125d6e4b
Mason Smith <masonsmith976@gmail.com>
2023-02-16 21:22:08 -0500

    another one

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-// "Hello World quentin"
+const button = document.getElementById('cocktail-button');
+// const drinkData = data.strDrink;
+
+
+
+button.addEventListener('click', () =>{
+    
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka')
+.then((response) => response.json())
+.then((data) => {
+    let drinkList = data.drinks;
+    for(let i = 0; i < drinkList.length; i++) {
+        // console.log(count);
+        // console.log(drinkList[i].strDrink);
+        drinkList.forEach(element => {
+            const div1 = document.createElement('div');
+            const drinkContainer = document.getElementById('content-container');
+            console.log(element.strDrink);
+            div1.innerText = (element.strDrink);
+            drinkContainer.append(div1)
+        });
+    }
+    
+   
+   
+   
+
+    
+    });
+//     const url1 = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka';
+    
+// async function getDrinks(${url1}){
+//     const x = await
+// }
+});
+
+
