f6d99f320ddce3a4e71daa48b8643372ce49b307
Addy Severe <adelaidecantave@gmail.com>
2023-02-23 12:00:35 -0500

    fix search functionality

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ const drinkBtn = document.getElementById('cocktail-button');
 const foodBtn = document.getElementById('food-button');
 const input = document.getElementById('query');
 const searchBtn = document.getElementById('search');
-let randomNum = Math.floor(Math.random() * 20 + 1); //<== random number between 1-20
-console.log(randomNum);
-searchBtn.addEventListener('input', () => {
-    fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=cae44d7078c3414a8f7abd3a8419364a&tags=vegetarian`)
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+    const searchTerm = input.value
+    //we need to prevent the form's  default beahavior of refreshing the page
+    event.preventDefault()
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?number=10&apiKey=${key}&includeIngredients=${searchTerm}`)
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        
     });
 });
 
 
-    const options = {
+const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '9b973d6284msh5c9a2e3f9b461eap1f9eb0jsna7b717b14f42',
@@ -22,42 +24,39 @@ searchBtn.addEventListener('input', () => {
 	}
 };
 
-drinkBtn.addEventListener('click', () =>{
-
-    //using the Query Parameter limit={randomNum} is not giving you a random drink, 
+drinkBtn.addEventListener('click', () => {
+    //Using the Query Parameter limit={randomNum} is not giving you a random drink, 
     //rather this is randomly giving you a different size list of drinks each api call
     //this needs to be changed to 
-fetch(`https://drinks-digital1.p.rapidapi.com/v1/cocktails`, options)
-	.then(response => response.json())
-	.then(data => {
+    fetch(`https://drinks-digital1.p.rapidapi.com/v1/cocktails`, options)
+        .then(response => response.json())
+        .then(data => {
 
-    // Data gives you a list of drinks, if you do data[0] in link 38 through 44 you will always get the first drink no matter what you do
-    // it is grabibng the drink in the list that is at the 0 position, the first drink.
-    // We need to randomize how we select a drink from the list rather than grabbing position zero. this is the purpose of
-    //funtion randomdrink
-        console.log(data);
-        const drink = randomdrink(data)
+        // Data gives you a list of drinks, if you do data[0] in link 38 through 44 you will always get the first drink no matter what you do
+        // it is grabbing the drink in the list that is at the 0 position, the first drink.
+        // We need to randomize how we select a drink from the list rather than grabbing position zero. this is the purpose of
+        //function randomdrink
+            console.log(data);
+            const drink = randomdrink(data)
 
-        const alcoholic = drink.alcoholic;
-        const cocktailName = drink.cocktail_name;
-        const description = drink.description;
-        const garnish = drink.garnish;
-//         // const ingredient = element.ingredients.ingredient.name;
-//         const img_src2 = element.glasses[0].glass.img_url;
-        const drinkContainer = document.getElementById('content-container1');
-        const div1 = document.createElement('div');
+            const alcoholic = drink.alcoholic;
+            const cocktailName = drink.cocktail_name;
+            const description = drink.description;
+            const garnish = drink.garnish;
+            const drinkContainer = document.getElementById('content-container1');
+            const div1 = document.createElement('div');
 
-        drinkContainer.innerHTML = `<div class="card w-60" style="width: 20rem; height: auto;">
-        
-        <h3>${cocktailName}</h3>
-        <p class="card text-center"><small class="text-muted">serves: ${garnish}</small></p>
-        <div class="card-body">
-            <p class="card-text"><small>${description}</small></p>
-            <p class="card-footer"><small class="text muted"${alcoholic}</small></p>
-        </div>
-        </div>`
-        drinkContainer.append(div1);
-    });
+            drinkContainer.innerHTML = `<div class="card w-60" style="width: 20rem; height: auto;">
+            
+            <h3>${cocktailName}</h3>
+            <p class="card text-center"><small class="text-muted">serves: ${garnish}</small></p>
+            <div class="card-body">
+                <p class="card-text"><small>${description}</small></p>
+                <p class="card-footer"><small class="text muted"${alcoholic}</small></p>
+            </div>
+            </div>`
+            drinkContainer.append(div1);
+        });
 });
 
 
@@ -75,7 +74,7 @@ function randomdrink (drinks) {
 
 foodBtn.addEventListener('click', () => {
     // clearContentContainer();
-    fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${key}&tags=vegetarian`)
+    fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${key}`)
     .then((response) => response.json())
     .then((data) => {
 
@@ -107,54 +106,10 @@ foodBtn.addEventListener('click', () => {
             <h3>${title}</h3>
             <p class="card text-center"><small class="text-muted">serves: ${servings}</small></p>
             <div class="card-body">
-              <p class="card-text"><small>${instructions}</small></p>
-              <p class="card-footer"><small class="text muted"${summary}</small></p>
+                <p class="card-text"><small>${instructions}</small></p>
+                <p class="card-footer"><small class="text muted"${summary}</small></p>
             </div>
           </div>`
         });
     });
 });
-
-
-
-
-
-function clearContentContainer(){
-    const contentContainerDiv = document.getElementById('content-container');
-    contentContainerDiv.innerHTML = "";
-}
-
-
-
-function addToContentContainer(element){
-    const contentContainerDiv = document.getElementById('content-container');
-    const foodOrDrinkDiv = document.createElement('div');
-    const img = document.createElement('img');
-    const h3 = document.createElement('h3');
-    const card_body_div = document.createElement('div');
-    const p = document.createElement('p');
-
-    img.src =  element.image;
-    img.className = "card-img-top"
-    img.alt = "..."
-
-    h3.innerHTML = element.title;
-
-    p.innerHTML = element.instructions;
-    p.className = "card-text"
-
-    card_body_div.className = "card-body"
-    card_body_div.appendChild(p);
-
-    foodOrDrinkDiv.className = "card"
-    foodOrDrinkDiv.style = "width: 18rem;"
-    foodOrDrinkDiv.appendChild(img);
-    foodOrDrinkDiv.appendChild(h3);
-    foodOrDrinkDiv.appendChild(card_body_div);
-
-    contentContainerDiv.appendChild(foodOrDrinkDiv);
-}
-
-
-
-
